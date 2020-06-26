072ab046dc7f6b6b973268d2030092c7164b33a2
Nivedita Prasad <56059682+Nivedita-coder@users.noreply.github.com>
2020-06-26 17:15:56 +0530

    Add files via upload

---
+++ build/precache-manifest.91c1377aec1902416f50726459584885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77952d997ced6c9996219b8c440fb76b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a29f7d577f7add69ff4",
+    "url": "/static/css/2.3f915d55.chunk.css"
+  },
+  {
+    "revision": "44a6ce465443b4a82851",
+    "url": "/static/css/main.363032cb.chunk.css"
+  },
+  {
+    "revision": "1a29f7d577f7add69ff4",
+    "url": "/static/js/2.63289f9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.63289f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a6ce465443b4a82851",
+    "url": "/static/js/main.68a61983.chunk.js"
+  },
+  {
+    "revision": "69d2b7e1484e517270ab",
+    "url": "/static/js/runtime-main.788e1730.js"
+  }
+]);
